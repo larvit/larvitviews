2c105b1f288c5817f90429b556a3988647066895
lillem4n <lilleman@larvit.se>
2015-09-25 00:04:16 +0200

    Fixed loads of stuff to remove the need for view files

--- larvitviews.js
+++ larvitviews.js
@@ -2,85 +2,27 @@
 
 var fs            = require('fs'),
     _             = require('underscore'),
-    path          = require('path'),
-    appPath       = path.dirname(require.main.filename),
     log           = require('winston'),
     compiledTmpls = {},
-    confFileName  = appPath + '/config/view.json',
-    larvitrouter  = require('larvitrouter')(),
-    confFile;
-
-log.debug('larvitviews: Check if config file, "' + confFileName + '", exists.');
-
-try {
-	fs.statSync(confFileName);
-	confFile = require(confFileName);
-	log.verbose('larvitviews: Config file "' + confFileName + '" loaded: ' + JSON.stringify(confFile));
-} catch(err) {
-	confFile = {
-		'formatDateTime': 'YYYY-MM-DD HH:mm',
-		'tmplPath':       'public/tmpl',
-		'viewPath':       'public'
-	};
-	log.verbose('larvitviews: Config file "' + confFileName + '" not found, loading defaults: ' + JSON.stringify(confFile));
-	log.debug('larvitviews: fs.statSync err: ' + err.message);
-}
+    larvitrouter  = require('larvitrouter')();
 
 /**
  * Compile templates and cache the compiled ones
  *
  * @param str staticFilename
- * @param func callback(err, compiledObj) - compiledObj is runnable
+ * @return func compileObj
  */
-function compileTmpl(staticFilename, callback) {
+function compileTmpl(staticFilename) {
+	var tmplFileContent;
+
 	if (compiledTmpls[staticFilename] === undefined) {
 		log.debug('larvitviews: compileTmpl() - Compiling previous uncompiled template "' + staticFilename + '"');
 
-		fs.readFile(staticFilename, 'utf8', function(err, tmplFileContent){
-			if ( ! err) {
-				compiledTmpls[staticFilename] = _.template(tmplFileContent);
-
-				callback(null, compiledTmpls[staticFilename]);
-			} else {
-				log.error('larvitviews: compileTmpl() - Could not compile template "' + staticFilename + '" err: ' + err.message);
-
-				callback(err);
-			}
-		});
-	} else {
-		callback(null, compiledTmpls[staticFilename]);
-	}
-}
-
-/**
- * Get a deep value from an object by a string path
- * For example:
- * var foo = {'bar': {'lurker': 'someValue'}}
- * getValByPath(foo, 'bar.lurker') returns 'someValue'
- *
- * @param obj obj
- * @param str path
- * @return mixed
- */
-function getValByPath(obj, path) {
-	var p;
-
-	if (typeof path === 'string') {
-		path = path.split('.');
+		tmplFileContent               = fs.readFileSync(staticFilename, 'utf8');
+		compiledTmpls[staticFilename] = _.template(tmplFileContent);
 	}
 
-	if (path.length > 1) {
-		p = path.shift();
-
-		if (typeof obj[p] === 'object') {
-			return getValByPath(obj[p], path);
-		} else {
-			return undefined;
-		}
-
-	} else {
-		return obj[path[0]];
-	}
+	return compiledTmpls[staticFilename];
 }
 
 exports = module.exports = function(options) {
@@ -88,114 +30,39 @@ exports = module.exports = function(options) {
 
 	// Copy options object - set default vars
 	options = _.extend({
-		'viewPath':      confFile.viewPath,
-		'tmplPath':      confFile.tmplPath,
-		'underscoreExt': {}
+		'formatDateTime': 'YYYY-MM-DD HH:mm',
+		'tmplPath':       'public/tmpl',
+		'underscoreExt':  {}
 	}, options);
-
-	// Extend underscore with the underscoreExt options
-	_ = _.extend(_, options.underscoreExt);
 
 	/**
 	 * Render template
 	 *
 	 * @param str tmplName - template name, without ".tmpl" and relative to options.tmplPath - will be looked for by larvitrouter.fileExists() recursively
 	 * @param obj data - data to be passed to the template rendering
-	 * @param func callback(null, string)
+	 * @return str
 	 */
-	returnObj.render = function(tmplName, data, callback) {
-		var tmplPath = options.tmplPath + '/' + tmplName + '.tmpl';
+	returnObj.render = function(tmplName, data) {
+		var tmplPath = options.tmplPath + '/' + tmplName + '.tmpl',
+		    tmplFullPath,
+		    compiled;
 
 		log.debug('larvitviews: render() - Trying to render "' + tmplName + '" with full path "' + tmplPath);
 
-		larvitrouter.fileExists(tmplPath, function(err, exists, fullPath) {
-			compileTmpl(fullPath, function(err, compiled) {
-				if (err) {
-					callback(err);
-					return;
-				}
+		tmplFullPath = larvitrouter.fileExists(tmplPath);
 
-				callback(null, compiled(data));
-			});
-		});
+		if (tmplFullPath !== false) {
+			compiled = compileTmpl(tmplFullPath);
+
+			return compiled(data);
+		}
 	};
 
-	/**
-	 * Render partials
-	 *
-	 * @param obj structure - what data should be where
-	 *                        [
-	 *                          {
-	 *                            'partName': 'foo',     // The name of this partial in the "master" template
-	 *                            'tmplPath': 'bar',     // The name of the template file to be loaded
-	 *                            'data':     'data.foo' // String representation of path to data for this partial
-	 *                          },
-	 *                          etc
-	 *                        ]
-	 * @param obj data - data structure - pointers in the structure points here
-	 * @param int partialNr - should be replaced by the callback in the initial call
-	 * @param func callback(err, tmplStr)
-	 */
-	returnObj.renderPartials = function(structure, data, partialNr, callback) {
-		var localData,
-		    partial,
-		    partialData;
+	// Extend underscore with the underscoreExt options
+	_ = _.extend(_, options.underscoreExt);
 
-		// If partialNr is not supplied, it should be the callback function
-		if (typeof partialNr === 'function') {
-			callback  = partialNr;
-			partialNr = 0;
-		}
-
-		partial = structure[partialNr];
-
-		localData = {
-			'tmplPath':  partial.tmplPath,
-			'tmplParts': data.tmplParts
-		};
-
-		if (partial.data !== undefined) {
-			partialData = getValByPath(data, partial.data);
-
-			if (partialData !== undefined && typeof partialData !== 'object') {
-				log.warn('larvitviews: renderPartials() - given partial data is not an object. Given path is "' + partial.data + '" and type is: "' + (typeof partialData) + '" with value: "' + partialData + '"');
-			}
-
-			if (partialData !== undefined) {
-				localData         = _.extend(partialData, localData);
-				localData._global = data._global;
-			} else {
-				log.warn('larvitviews: renderPartials() - getValByPath() on partial.data: "' + partial.data + '" failed. Most common reason for this error is the view structure is done in the wrong order.');
-			}
-		} else {
-			localData = _.extend(data, localData);
-		}
-
-		// If a _global magic object key exists, always attach it to the local data
-		if (data._global !== undefined) {
-			localData._global = data._global;
-		} else {
-			// Always make sure the global is set not to cause undefined errors in templates
-			localData._global = {};
-		}
-
-		returnObj.render(partial.tmplPath, localData, function(err, tmplStr) {
-			if (err) {
-				callback(err);
-				return;
-			}
-
-			data.tmplParts[partial.partName] = tmplStr;
-
-			partialNr ++;
-
-			if (structure[partialNr] !== undefined) {
-				returnObj.renderPartials(structure, data, partialNr, callback);
-			} else {
-				callback(null, tmplStr);
-			}
-		});
-	};
+	// Extend underscore with the render method, so we can render other templates from within it.
+	_ = _.extend(_, {'render': returnObj.render});
 
 	return returnObj;
 };
