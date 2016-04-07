870a299651c899a75afdb11a6a9a9f6a3af1f12c
Lilleman <lilleman@larvit.se>
2016-04-07 18:08:38 +0200

    Fixed so partials actually works again

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const lfs = require('larvitfs'),
 let _ = require('lodash');
 
 function Instance(options) {
-	this.compiledTmpls = new Map();
-
 	// Copy options object - set default vars
 	this.options = _.extend({
 		'formatDateTime': 'YYYY-MM-DD HH:mm',
@@ -22,83 +20,92 @@ function Instance(options) {
 	_ = _.extend(_, this.options.lodashExt);
 
 	// Extend lodash with the render method, so we can render other templates from within it.
-	_ = _.extend(_, {'render': this.render});
+	_.render = renderInstance(this.options);
+	//_ = _.extend(_, {'render': this.render});
 }
 
-/**
- * Compile templates and cache the compiled ones
- *
- * @param str staticFilename - Full exact path
- * @return func compileObj or false
- */
-Instance.prototype.compileTmpl = function compileTmpl(staticFilename) {
-	if (this.compiledTmpls.get(staticFilename) === undefined) {
-		let tmplFileContent;
-
-		log.debug('larvitviews: compileTmpl() - Compiling previous uncompiled template "' + staticFilename + '"');
-
-		try {
-			tmplFileContent = fs.readFileSync(staticFilename, 'utf8');
-		} catch(err) {
-			log.warn('larvitviews: compileTmpl() - Error reading file "' + staticFilename + '" from disk. Error: ' + err.message);
-			return false;
-		}
-
-		try {
-			this.compiledTmpls.set(staticFilename, _.template(tmplFileContent));
-		} catch(err) {
-			log.warn('larvitviews: compileTmpl() - Could not compile template from file "' + staticFilename + '". Error: ' + err.message);
-			return false;
-		}
-	} else {
-		log.silly('larvitviews: compileTmpl() - Template "' + staticFilename + '" already compiled');
-	}
-
-	return this.compiledTmpls.get(staticFilename);
+Instance.prototype.render = function(fileName, data) {
+	return renderInstance(this.options)(fileName, data);
 };
 
-/**
- * Render template or other file
- *
- * @param str fileName - filename to render. Will search for exact match first and then with added .tmpl, relative to options.tmplPath - will be looked for by lfs.getPathSync()
- * @param obj data - data to be passed to the template rendering
- * @return str or false
- */
-Instance.prototype.render = function render(fileName, data) {
-	const tmplPath = this.options.tmplPath + '/' + fileName;
+function renderInstance(options) {
+	const compiledTmpls = new Map();
 
-	let tmplFullPath;
+	/**
+	 * Compile templates and cache the compiled ones
+	 *
+	 * @param str staticFilename - Full exact path
+	 * @return func compileObj or false
+	 */
+	function compileTmpl(staticFilename) {
+		if (compiledTmpls.get(staticFilename) === undefined) {
+			let tmplFileContent;
 
-	log.debug('larvitviews: render() - Trying to render "' + fileName + '" with path "' + tmplPath);
+			log.debug('larvitviews: renderInstance() - compileTmpl() - Compiling previous uncompiled template "' + staticFilename + '"');
 
-	tmplFullPath = lfs.getPathSync(tmplPath);
+			try {
+				tmplFileContent = fs.readFileSync(staticFilename, 'utf8');
+			} catch(err) {
+				log.warn('larvitviews: renderInstance() - compileTmpl() - Error reading file "' + staticFilename + '" from disk. Error: ' + err.message);
+				return false;
+			}
 
-	// If the exact filename was not found, look for a file with added .tmpl at the end
-	if (tmplFullPath === false)
-		tmplFullPath = lfs.getPathSync(tmplPath + '.tmpl');
-
-	if (tmplFullPath !== false) {
-		let compiledStr,
-		    compiled;
-
-		compiled = this.compileTmpl(tmplFullPath);
-
-		if ( ! compiled) {
-			return false;
+			try {
+				compiledTmpls.set(staticFilename, _.template(tmplFileContent));
+			} catch(err) {
+				log.warn('larvitviews: renderInstance() - compileTmpl() - Could not compile template from file "' + staticFilename + '". Error: ' + err.message);
+				return false;
+			}
+		} else {
+			log.silly('larvitviews: renderInstance() - compileTmpl() - Template "' + staticFilename + '" already compiled');
 		}
 
-		try {
-			compiledStr = compiled(data);
-		} catch(err) {
-			log.warn('larvitviews: render() - Could not render "' + fileName + '". Error: ' + err.message);
-			return 'Error: ' + err.message;
+		return compiledTmpls.get(staticFilename);
+	};
+
+	/**
+	 * Render template or other file
+	 *
+	 * @param str fileName - filename to render. Will search for exact match first and then with added .tmpl, relative to options.tmplPath - will be looked for by lfs.getPathSync()
+	 * @param obj data - data to be passed to the template rendering
+	 * @return str or false
+	 */
+	return function render(fileName, data) {
+		const tmplPath = options.tmplPath + '/' + fileName;
+
+		let tmplFullPath;
+
+		log.debug('larvitviews: renderInstance() - render() - Trying to render "' + fileName + '" with path "' + tmplPath);
+
+		tmplFullPath = lfs.getPathSync(tmplPath);
+
+		// If the exact filename was not found, look for a file with added .tmpl at the end
+		if (tmplFullPath === false)
+			tmplFullPath = lfs.getPathSync(tmplPath + '.tmpl');
+
+		if (tmplFullPath !== false) {
+			let compiledStr,
+			    compiled;
+
+			compiled = compileTmpl(tmplFullPath);
+
+			if ( ! compiled) {
+				return false;
+			}
+
+			try {
+				compiledStr = compiled(data);
+			} catch(err) {
+				log.warn('larvitviews: renderInstance() - render() - Could not render "' + fileName + '". Error: ' + err.message);
+				return 'Error: ' + err.message;
+			}
+
+			return compiledStr;
 		}
 
-		return compiledStr;
-	}
-
-	return false;
-};
+		return false;
+	};
+}
 
 exports = module.exports = function(options) {
 	return new Instance(options);
--- test/test.js
+++ test/test.js
@@ -4,22 +4,12 @@ const assert = require('assert'),
       views  = require(__dirname + '/../index.js')();
 
 describe('Compile templates', function() {
-	it('should compile a template and verify its working', function(done) {
-		const tmplFile     = __dirname + '/../public/tmpl/test.tmpl',
-		      compiledTmpl = views.compileTmpl(tmplFile),
-		      tmplData     = {'foo': 'bar'},
-		      compiledStr  = compiledTmpl(tmplData);
-
-		assert.deepEqual(compiledStr, 'Lur lurr bar', 'Should return "Lur lurr bar" but returned "' + compiledStr + '"');
-		done();
-	});
-
 	it('should use the render function', function(done) {
 		const tmplName    = 'test',
 		      tmplData    = {'foo': 'bar'},
 		      compiledStr = views.render(tmplName, tmplData);
 
-		assert.deepEqual(compiledStr, 'Lur lurr bar', 'Should return "Lur lurr bar" but returned "' + compiledStr + '"');
+		assert.deepEqual(compiledStr, 'Lur lurr bar ((fjomp))', 'Should return "Lur lurr bar ((fjomp))" but returned "' + compiledStr + '"');
 		done();
 	});
 });
